727588ef0a18c4a45e0578c13f94f3bf8a4793da
gburns1986 <gburns1986@gmail.com>
2014-01-28 16:00:31 +0000

    Poll and Choice models added, polls app added to settings.py

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+import datetime
+
+#Poll contains a question and a publication date
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __unicode__(self):
+        return self.question
+    
+    #custom method
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+#Each choice is associated with a Poll
+#Contains a tally and a choice
+class Choice(models.Model):
+    #ForiegnKey tells django that each choice
+    #is related to a single Poll
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.choice_text
