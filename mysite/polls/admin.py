a9c6df3389b19d0d03218498a963b80b371d267b
gburns1986 <gburns1986@gmail.com>
2014-01-28 17:59:30 +0000

    Admin configured for poll and choice models

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from polls.models import Poll, Choice
 
-# Register your models here.
+#this class will allow the Chouice model
+#to be displayed on the add Poll admin page
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question',               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        ]
+    #tells django choices are edited on the Poll page
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Poll, PollAdmin)
