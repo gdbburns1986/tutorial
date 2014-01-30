3d15eebac3754064632a0692f430730e03b8546a
gburns1986 <gburns1986@gmail.com>
2014-01-30 20:43:58 +0000

    simple views added

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("This is the polls index page")
+
+def detail(request, poll_id):
+    return HttpResponse("This is poll number %s" % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
